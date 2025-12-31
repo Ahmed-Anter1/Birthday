4d9bf2221fcea469d35e095b499a31aa967782d0
ahmedanter <ahmedanter1@gmail.com>
2025-12-31 18:35:06 +0200

    fix5

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://birthday-amdq.vercel.app'
+    'https://birthday-amdq-rktxz568z-ahmed-anters-projects.vercel.app'
   ],
   credentials: true
 }));
