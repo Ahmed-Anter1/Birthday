87dd1a9012f116eac7a66059db7de19fe0d13f87
ahmedanter <ahmedanter1@gmail.com>
2025-12-31 18:55:47 +0200

    fix7

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -16,5 +16,5 @@ const fileFilter = (req, file, cb) => {
 export const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 100 * 1024 * 1024 }
+  limits: { fileSize: 20 * 1024 * 1024 }
 });
